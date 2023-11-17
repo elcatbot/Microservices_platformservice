8a12feebf31fa0b7f7fefd0ead76cb2c37118f47
elcatbot <viajero3019@gmail.com>
2023-11-17 18:25:14 -0500

    Grpc Service added

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using CommandsService.DTOs;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -13,6 +14,10 @@ namespace CommandsService.Profiles
             CreateMap<CommandCreateDto, Command>();
             CreateMap<Command, CommandReadDto>();
             CreateMap<PlatformPublishedDto, Platform>().ForMember(desti => desti.ExternalId, opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(desti => desti.ExternalId, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(desti => desti.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(desti => desti.Commands, opt => opt.Ignore());
         }
     }
 }
--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,11 +32,19 @@ namespace CommandsService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+
             services.AddScoped<ICommandRepo, CommandRepo>();
+
             services.AddControllers();
+
             services.AddHostedService<MessageBusSubscriber>();
+
             services.AddSingleton<IEventProcessor, EventProcessor>();
+
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<IPlatformDataClient, PlatformDataClient>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
@@ -62,6 +71,8 @@ namespace CommandsService
             {
                 endpoints.MapControllers();
             });
+
+            PrepDb.PrepPopulation(app);
         }
     }
 }
